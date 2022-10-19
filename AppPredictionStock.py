42ccded5075ee1a7393061b76ac25d4114a1b670
JaroslavKotrba <65077222+JaroslavKotrba@users.noreply.github.com>
2022-10-19 08:00:15 +0200

    Update AppPredictionStock.py

--- AppPredictionStock.py
+++ AppPredictionStock.py
@@ -9,7 +9,7 @@ import streamlit as st
 from datetime import date
 
 import yfinance as yf
-from fbprophet import Prophet
+from prophet import Prophet # prophet not fbprophet
 # from fbprophet.plot import plot_plotly
 from plotly import graph_objs as go
 import pandas as pd
